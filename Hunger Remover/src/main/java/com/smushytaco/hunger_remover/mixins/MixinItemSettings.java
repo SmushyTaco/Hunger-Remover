7bfc81893d17ef7bd4ba2d68ac98fed2881a3474
SmushyTaco <smushytaco@gmail.com>
2021-05-14 22:19:34 -0700

    Dependency Updates

--- Hunger Remover/src/main/java/com/smushytaco/hunger_remover/mixins/MixinItemSettings.java
+++ Hunger Remover/src/main/java/com/smushytaco/hunger_remover/mixins/MixinItemSettings.java
@@ -13,7 +13,7 @@ public abstract class MixinItemSettings {
     @Shadow
     private FoodComponent foodComponent;
     @Inject(method = "food", at = @At("HEAD"))
-    private void onSetFoodComponent(FoodComponent foodComponent, CallbackInfoReturnable<Item.Settings> info) {
+    private void hookFood(FoodComponent foodComponent, CallbackInfoReturnable<Item.Settings> info) {
         HungerRemover.INSTANCE.initializeConfig();
         if (HungerRemover.INSTANCE.getConfig().getDisableMod()) return;
         if (HungerRemover.INSTANCE.getConfig().getFoodCantStack()) {
@@ -23,7 +23,7 @@ public abstract class MixinItemSettings {
         }
     }
     @ModifyVariable(method = "maxCount", at = @At("HEAD"))
-    private int adjustMaxCount(int maxCount) {
+    private int hookMaxCount(int maxCount) {
         HungerRemover.INSTANCE.initializeConfig();
         if (HungerRemover.INSTANCE.getConfig().getDisableMod()) return maxCount;
         if (HungerRemover.INSTANCE.getConfig().getFoodCantStack()) {
