b26c4a942bcfadf20f39db9869604965f4da8784
Nikan Radan <github.com@alias.nikanradan.com>
2021-06-08 16:58:41 -0700

    1.17 Update

--- Hunger Remover/src/main/kotlin/com/smushytaco/hunger_remover/NoHungerManager.kt
+++ Hunger Remover/src/main/kotlin/com/smushytaco/hunger_remover/NoHungerManager.kt
@@ -3,16 +3,16 @@ import net.minecraft.entity.player.HungerManager
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
-import net.minecraft.nbt.CompoundTag
+import net.minecraft.nbt.NbtCompound
 class NoHungerManager(private val player: PlayerEntity) : HungerManager() {
-    override fun setSaturationLevelClient(saturationLevel: Float) {}
+    override fun setSaturationLevel(saturationLevel: Float) {}
     override fun isNotFull() = player.health < player.maxHealth
     override fun update(playerEntity: PlayerEntity) {}
     override fun addExhaustion(exhaustion: Float) {}
     override fun getFoodLevel() = MAX_FOOD_LEVEL
     override fun setFoodLevel(foodLevel: Int) {}
-    override fun fromTag(tag: CompoundTag) {}
-    override fun toTag(tag: CompoundTag) {}
+    override fun readNbt(nbt: NbtCompound) {}
+    override fun writeNbt(nbt: NbtCompound?) {}
     private fun add(health: Int) {
         player.heal(health.toFloat())
     }
