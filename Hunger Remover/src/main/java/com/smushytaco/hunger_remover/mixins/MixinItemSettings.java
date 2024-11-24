542ba6a7e5b1170647a7751ee2b97c0e9002c34d
Nikan Radan <personal@nikanradan.com>
2024-11-23 17:03:21 -0800

    No More Restarts + Better Customization

--- Hunger Remover/src/main/java/com/smushytaco/hunger_remover/mixins/MixinItemSettings.java
+++ Hunger Remover/src/main/java/com/smushytaco/hunger_remover/mixins/MixinItemSettings.java
@@ -3,6 +3,7 @@ import com.smushytaco.hunger_remover.HungerRemover;
 import net.minecraft.component.ComponentMap;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.ConsumableComponent;
+import net.minecraft.component.type.ConsumableComponents;
 import net.minecraft.component.type.FoodComponent;
 import net.minecraft.item.Item;
 import org.jetbrains.annotations.Nullable;
@@ -21,12 +22,12 @@ public abstract class MixinItemSettings {
     private ComponentMap.Builder components;
     @Inject(method = "food(Lnet/minecraft/component/type/FoodComponent;Lnet/minecraft/component/type/ConsumableComponent;)Lnet/minecraft/item/Item$Settings;", at = @At("HEAD"))
     private void hookFood(FoodComponent foodComponent, ConsumableComponent consumableComponent, CallbackInfoReturnable<Item.Settings> cir) {
-        if (HungerRemover.INSTANCE.getConfig().getDisableMod() || !HungerRemover.INSTANCE.getConfig().getFoodCantStack() || foodComponent == null) return;
-        ((Item.Settings) (Object) this).maxCount(1);
+        if (components == null || HungerRemover.INSTANCE.getConfig().getDisableMod() || !HungerRemover.INSTANCE.getConfig().getModifyMaxFoodStackCount() || consumableComponent != ConsumableComponents.FOOD) return;
+        if ((int) ((ComponentMapBuilderAccessor) components).getComponents().getOrDefault(DataComponentTypes.MAX_STACK_SIZE, 64) > HungerRemover.INSTANCE.getConfig().getMaxFoodStackCount()) ((Item.Settings) (Object) this).maxCount(HungerRemover.INSTANCE.getConfig().getMaxFoodStackCount());
     }
     @ModifyVariable(method = "maxCount", at = @At("HEAD"), argsOnly = true)
     private int hookMaxCount(int maxCount) {
         HungerRemover.INSTANCE.initializeConfig();
-        return HungerRemover.INSTANCE.getConfig().getDisableMod() || !HungerRemover.INSTANCE.getConfig().getFoodCantStack() || components == null || !((ComponentMapBuilderAccessor) components).getComponents().containsKey(DataComponentTypes.FOOD) ? maxCount : 1;
+        return HungerRemover.INSTANCE.getConfig().getDisableMod() || !HungerRemover.INSTANCE.getConfig().getModifyMaxFoodStackCount() || components == null || (int) ((ComponentMapBuilderAccessor) components).getComponents().getOrDefault(DataComponentTypes.MAX_STACK_SIZE, 64) <= HungerRemover.INSTANCE.getConfig().getMaxFoodStackCount() || !((ComponentMapBuilderAccessor) components).getComponents().containsKey(DataComponentTypes.FOOD) ? maxCount : HungerRemover.INSTANCE.getConfig().getMaxFoodStackCount();
     }
 }
