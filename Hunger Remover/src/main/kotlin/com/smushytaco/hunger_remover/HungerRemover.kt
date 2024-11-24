430cf328ce54a1228b60f87a32524daca6d55017
Nikan Radan <personal@nikanradan.com>
2024-11-23 23:40:30 -0800

    New Config Library For Server Client Sync

--- Hunger Remover/src/main/kotlin/com/smushytaco/hunger_remover/HungerRemover.kt
+++ Hunger Remover/src/main/kotlin/com/smushytaco/hunger_remover/HungerRemover.kt
@@ -1,20 +1,9 @@
 package com.smushytaco.hunger_remover
-import com.smushytaco.hunger_remover.configuration_support.ModConfiguration
-import me.shedaniel.autoconfig.AutoConfig
-import me.shedaniel.autoconfig.annotation.Config
-import me.shedaniel.autoconfig.serializer.GsonConfigSerializer
 import net.fabricmc.api.ModInitializer
 object HungerRemover : ModInitializer {
     const val MAX_FOOD_LEVEL = 20
     const val MAX_SATURATION_LEVEL = MAX_FOOD_LEVEL.toFloat()
     const val MOD_ID = "hunger_remover"
-    lateinit var config: ModConfiguration
-        private set
+    val config = ModConfig.createAndLoad()
     override fun onInitialize() {}
-    fun initializeConfig() {
-        AutoConfig.register(ModConfiguration::class.java) { definition: Config, configClass: Class<ModConfiguration> ->
-            GsonConfigSerializer(definition, configClass)
-        }
-        config = AutoConfig.getConfigHolder(ModConfiguration::class.java).config
-    }
 }
--- Hunger Remover/src/main/kotlin/com/smushytaco/hunger_remover/HungerRemoverPreLaunch.kt
+++
@@ -1,3 +0,0 @@
-package com.smushytaco.hunger_remover
-import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint
-object HungerRemoverPreLaunch: PreLaunchEntrypoint { override fun onPreLaunch() { HungerRemover.initializeConfig() } }
