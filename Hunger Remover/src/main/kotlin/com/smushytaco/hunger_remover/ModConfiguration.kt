e7c3de2bb5757fd0e9cecc75a6583495408ab7ba
Nikan Radan <github.com@alias.nikanradan.com>
2024-11-23 23:40:30 -0800

    New Config Library For Server Client Sync

---
+++ Hunger Remover/src/main/kotlin/com/smushytaco/hunger_remover/ModConfiguration.kt
@@ -0,0 +1,36 @@
+package com.smushytaco.hunger_remover
+import io.wispforest.owo.config.Option
+import io.wispforest.owo.config.annotation.*
+@Modmenu(modId = HungerRemover.MOD_ID)
+@Config(name = HungerRemover.MOD_ID, wrapperName = "ModConfig")
+@Suppress("UNUSED")
+class ModConfiguration {
+    @JvmField
+    @Sync(Option.SyncMode.OVERRIDE_CLIENT)
+    var disableMod = false
+    @JvmField
+    @Sync(Option.SyncMode.OVERRIDE_CLIENT)
+    var eatInstantly = false
+    @JvmField
+    @Sync(Option.SyncMode.OVERRIDE_CLIENT)
+    var modifyMaxFoodStackCount = false
+    @JvmField
+    @PredicateConstraint(value = "maxFoodStackCountValidation")
+    @Sync(Option.SyncMode.OVERRIDE_CLIENT)
+    var maxFoodStackCount = 1
+    @JvmField
+    @Sync(Option.SyncMode.OVERRIDE_CLIENT)
+    var cantSprint = true
+    @JvmField
+    var hideHungerBar = true
+    @JvmField
+    var moveArmorBarToHungerBar = true
+    @JvmField
+    var replaceAllHungerWithPoison = true
+    @JvmField
+    var replaceHungerFromFoodWithPoison = true
+    companion object {
+        @JvmStatic
+        fun maxFoodStackCountValidation(maxFoodStackCount: Int) = maxFoodStackCount > 0
+    }
+}
